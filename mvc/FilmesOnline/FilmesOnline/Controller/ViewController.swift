f71f91abe6e0ba02ca91715deb055e2e71170ca5
Leonardo Simas <leosimasgoncalves@gmail.com>
2019-10-10 22:04:08 -0300

    criacao do projeto mvc

---
+++ mvc/FilmesOnline/FilmesOnline/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilmesOnline
+//
+//  Created by SoSucesso on 10/10/19.
+//  Copyright © 2019 Leonardo Simas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
