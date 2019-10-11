ecc14f3456e2808f7822812fcba342f43c9fc086
leosimas <leosimasgoncalves@gmail.com>
2019-10-11 13:48:09 -0300

    atualizacao do gitignore

---
+++ mvc/FilmesOnline/FilmesOnline/Model/Bean/Filme.swift
@@ -0,0 +1,9 @@
+//
+//  Filme.swift
+//  FilmesOnline
+//
+//  Created by ssa on 11/10/19.
+//  Copyright © 2019 Leonardo Simas. All rights reserved.
+//
+
+import Foundation
---
+++ mvc/FilmesOnline/FilmesOnline/MovieDB.swift
@@ -0,0 +1,9 @@
+//
+//  MovieDB.swift
+//  FilmesOnline
+//
+//  Created by ssa on 11/10/19.
+//  Copyright © 2019 Leonardo Simas. All rights reserved.
+//
+
+import Foundation
